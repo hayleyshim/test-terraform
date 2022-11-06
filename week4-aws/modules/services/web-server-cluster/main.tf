209d1b498c61d77ddec5c43d45c1233b2bd0bfcf
hayleyshim <yhshim17@gmail.com>
2022-11-06 21:25:22 +0900

    week4 update

---
+++ week4-aws/modules/services/web-server-cluster/main.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
+
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
+
+  min_size = var.min_size
+  max_size = var.max_size
+
+  tag {
+    key                 = "Name"
+    value               = var.cluster_name
+    propagate_at_launch = true
+  }
+}
+
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 3
+  desired_capacity      = 10
+  recurrence            = "0 8 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 3
+  desired_capacity      = 2
+  recurrence            = "0 21 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
